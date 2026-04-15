519ea8a3d05a5003d1563da84c99cf95ef37f9e5
mizan <mmizan24@gmail.com>
2026-04-15 15:55:26 +0600

    Adding some required package

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <n
+    
     <h2 className="text-2xl font-bold text-gray-900 bg-amber-700 text-align-center">Welcome to KeenKeeper</h2>
   );
 }
